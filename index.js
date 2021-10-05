02f776bf193470ae6934bbdfbf1e7635322f65cd
Fernando Lira <it.fernandolira@gmail.com>
2021-10-05 19:42:25 +0100

    Model Respostas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
+
 //Database
 connection
     .authenticate()
