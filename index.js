f9646cc1a8c5f21b10fb92bd31cc95459975bfc0
Fernando Lira <it.fernandolira@gmail.com>
2021-10-05 11:56:10 +0100

    Ficheiros estáticos - CSS e Imagens

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 
 // Instrução para o Express usar o EJS como View engine
 app.set('view engine','ejs');
+app.use(express.static('public'));
 
 app.get("/:nome/:lang",function(req,res){
     //res.send("<h1>Bem vindo à plataforma de Perguntas e Respostas</h1>");
