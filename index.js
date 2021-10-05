4d091b138d7c552b62831032368a6abedb609b29
Fernando Lira <it.fernandolira@gmail.com>
2021-10-05 19:56:54 +0100

    Listar as respostas

--- index.js
+++ index.js
@@ -86,13 +86,34 @@ app.get("/pergunta/:id",(req ,res) => {
         where: {id: id}
     }).then(pergunta => {
         if(pergunta != undefined){ // Pergunta encontrada
-            res.render("pergunta",{
-                pergunta: pergunta
+            
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order:[ 
+                    ['id','DESC'] 
+                ]
+            }).then(respostas => {
+                res.render("pergunta",{
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
             });
+
         }else{ // Não encontrada
             res.redirect("/");
         }
     });
 })
+
+app.post("/responder",(req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/"+perguntaId);
+    });
+});
 
 app.listen(4000,()=>{console.log("Servidor ativo!");});
