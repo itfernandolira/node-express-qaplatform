1ee66b25002db05fbd86a9f9bde87ef4387a890a
Fernando Lira <it.fernandolira@gmail.com>
2021-10-05 11:13:08 +0100

    Iniciar o Express e definir o EJS como View Engine

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+// Instrução para o Express usar o EJS como View engine
+app.set('view engine','ejs');
+
+app.get("/",function(req,res){
+    res.send("<h1>Bem vindo à plataforma de Perguntas e Respostas</h1>");
+});
+
+app.listen(8080,()=>{console.log("Servidor ativo!");});
